9b9d499d7038722d92a8e9eaa83366a2f15b2c70
Heinermann <aheinerm@gmail.com>
2021-03-29 02:23:14 -0700

    Fix teamcolor to work in Factorio

--- anim2factorio/convert.cpp
+++ anim2factorio/convert.cpp
@@ -37,7 +37,7 @@ void bitBlit32(const std::vector<std::uint8_t>& src, int srcx, int srcy, int src
   }
 }
 
-void toSpritesheet(const anim& anim_parent, dds_img_t& img, gfx_info_t& img_info) {
+void toSpritesheet(const anim& anim_parent, dds_img_t& img, gfx_info_t* img_info = nullptr) {
   // Get compacted grp width/height to reduce image size
   std::uint16_t grpWidth = 0;
   std::uint16_t grpHeight = 0;
@@ -71,10 +71,36 @@ void toSpritesheet(const anim& anim_parent, dds_img_t& img, gfx_info_t& img_info
   img.width = newImgWidth;
   img.height = newImgHeight;
 
-  img_info.frame_width = grpWidth;
-  img_info.frame_height = grpHeight;
-  img_info.line_length = targetCellsPerRow;
-  img_info.frame_count = int(anim_parent.framedata.size());
+  if (img_info != nullptr) {
+    img_info->frame_width = grpWidth;
+    img_info->frame_height = grpHeight;
+    img_info->line_length = targetCellsPerRow;
+    img_info->frame_count = int(anim_parent.framedata.size());
+  }
+}
+
+void convert_img(anim& anim_data, const std::string& layer, gfx_info_t* result_info = nullptr) {
+  swapRGB(anim_data.data.at(layer));
+  toSpritesheet(anim_data, anim_data.data.at(layer), result_info);
+}
+
+bool has_layer(const anim& anim_data, const std::string& layer) {
+  return anim_data.data.count(layer) > 0;
+}
+
+void write_png(const std::string& filename, const anim& anim_data, const std::string& layer) {
+  const dds_img_t& img = anim_data.data.at(layer);
+  int pngerr = lodepng_encode32_file(filename.c_str(), img.data.data(), img.width, img.height);
+  if (pngerr != 0) {
+    std::cerr << "PNG err: " << pngerr << std::endl;
+    throw std::exception("Failed to write PNG");
+  }
+}
+
+void apply_mask(dds_img_t& src, dds_img_t& dst_mask) {
+  for (unsigned i = 0; i < dst_mask.data.size(); ++i) {
+    dst_mask.data[i] &= src.data[i];
+  }
 }
 
 void convert(const std::string& input, const std::string& output_dir) {
@@ -84,28 +110,37 @@ void convert(const std::string& input, const std::string& output_dir) {
 
   // Create PNG file
   std::string out_file_prefix = (std::filesystem::path(output_dir) / std::filesystem::path(input).stem()).string();
-  for (auto& anim_pair : anim_data.data) {
-    dds_img_t& img = anim_pair.second;
-    gfx_info_t result_info;
 
-    swapRGB(img);
-    toSpritesheet(anim_data, img, result_info);
+  bool has_diffuse = has_layer(anim_data, "diffuse");
+  bool has_teamcolor = has_layer(anim_data, "teamcolor");
+  bool has_light = has_layer(anim_data, "emissive");
 
-    std::string outfile = out_file_prefix + "_" + anim_pair.first + ".png";
-    int pngerr = lodepng_encode32_file(outfile.c_str(), img.data.data(), img.width, img.height);
-    if (pngerr != 0) {
-      std::cerr << "PNG err: " << pngerr << std::endl;
-      throw std::exception("Failed to write PNG");
-    }
+  if (!has_diffuse) return;
+  
+  gfx_info_t result_info;
+  convert_img(anim_data, "diffuse", &result_info);
+  write_png(out_file_prefix + "_diffuse.png", anim_data, "diffuse");
 
-    std::ofstream json(outfile + ".lua");
-    json << "{\n"
-      << "  filename = \"__starcraft__/graphics/" << std::filesystem::path(outfile).filename() << "\",\n"
-      << "  size = {" << result_info.frame_width << ", " << result_info.frame_height << "},\n"
-      << "  line_length = " << result_info.line_length << ",\n"
-      << "  frame_count = " << result_info.frame_count << ",\n"
-      << "  scale = 0.5\n"
-      << "}\n";
+  if (has_teamcolor) {
+    convert_img(anim_data, "teamcolor", &result_info);
+    apply_mask(anim_data.data.at("diffuse"), anim_data.data.at("teamcolor"));
+    write_png(out_file_prefix + "_teamcolor.png", anim_data, "teamcolor");
   }
+
+  if (has_light) {
+    convert_img(anim_data, "emissive", &result_info);
+    write_png(out_file_prefix + "_emissive.png", anim_data, "emissive");
+  }
+
+  /* TODO
+  std::ofstream json(outfile + ".lua");
+  json << "{\n"
+    << "  filename = \"__starcraft__/graphics/" << std::filesystem::path(outfile).filename().string() << "\",\n"
+    << "  size = {" << result_info.frame_width << ", " << result_info.frame_height << "},\n"
+    << "  line_length = " << result_info.line_length << ",\n"
+    << "  frame_count = " << result_info.frame_count << ",\n"
+    << "  scale = 0.5\n"
+    << "}\n";
+    */
 }
 
